4272a912f90dcafa481ed2d145a34f61c358fed5
teacoder <stewart.jamie.d@googlemail.com>
2025-04-29 15:55:26 +0100

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,5 +1,7 @@
+var navType = document.getElementsByName('navtype')[0].content;
 const nav = document.querySelector( '.navBar' )
 fetch( '/navbar.html')
 .then( res=>res.text())
 .then(data=>{
 	nav.innerHTML=data})
+.then( document.getElementById(navType).classList.add("main-navigation-button-active")
